587f891d6a6d528e47f86ec26ea8c0e0acdf1e02
Farouq <farouqimoro@gmail.com>
2024-09-01 21:22:47 +0000

    0-simple_helper

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -2,6 +2,6 @@
 """This contains a helper function for pagination."""
 
 
-def index_range(page: int, page_size: int ) -> tuple:
+def index_range(page: int, page_size: int) -> tuple:
     """This function returns the page indexes for a page and page_size"""
     return (page - 1) * page_size, page * page_size
