c849c31502694eaa39868cc78428623fd399e28d
Farouq <farouqimoro@gmail.com>
2024-09-01 21:11:52 +0000

    0-simple_helper

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,8 +1,8 @@
+#!/usr/bin/python3
 """
 This contains a helper function for pagination.
 """
 
 def index_range(page, page_size):
+    """This function returns the page indexes for a page and page_size"""
     return ((page - 1) * page_size, (page - 1) * page_size + page_size)
-
-print(index_range(1, 7))
