0090e81830acc5df2434ce05c223024daf811f7d
Farouq <farouqimoro@gmail.com>
2024-09-04 23:22:40 +0000

    create /home/farouq/Desktop/ALX/hack/alx-backend/0x01-caching/0-basic_cache.py

--- 0x01-caching/alx_hack.sh
+++ 0x01-caching/alx_hack.sh
@@ -4,16 +4,17 @@
 
 cd ~/Desktop/ALX
 mkdir hack
+cd hack
 
 if [ -d "$1" ]; then
     echo "$1 already exists. Transferring files"
 else
-    git clone "https:/github.com/hcissey0/$1"
+    git clone "https://github.com/hcissey0/$1"
 fi
 
-cd "$1/$2"
+cd ~/Desktop/ALX/$1/$2
 
-for file in *; do
+for file in ~/Desktop/ALX/hack/$1/$2/*; do
     mv $file ~/Desktop/ALX/$1/$2
     git add .
     git commit -m "create $file"
